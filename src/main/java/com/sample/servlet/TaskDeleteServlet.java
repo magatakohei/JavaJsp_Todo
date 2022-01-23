e03eb93cafa547b7bc985f0ef3b060ae1a9c14b1
magako <qutianh@gmail.com>
2022-01-23 21:59:47 +0900

    add:タスクの削除処理の追加

---
+++ src/main/java/com/sample/servlet/TaskDeleteServlet.java
@@ -0,0 +1,58 @@
+package com.sample.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.sample.model.Task;
+
+/**
+ * Servlet implementation class TaskDeleteServlet
+ */
+@WebServlet("/delete")
+public class TaskDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TaskDeleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		int id = Integer.parseInt(request.getParameter("id"));
+		List<Task> taskList = (List<Task>) session.getAttribute("taskList");
+		
+		taskList.removeIf(task->task.getId() == id);
+		
+		session.setAttribute("taskList", taskList);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/list");
+		rd.forward(request, response);
+		
+		return;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
